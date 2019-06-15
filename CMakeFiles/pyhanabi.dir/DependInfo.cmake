dbf0c5cd81b9d151a8d245c346a50895f63835d9
hellovertex <sascha.lange@campus.tu-berlin.de>
2019-06-15 19:29:35 +0200

    add project files for master merge

---
+++ CMakeFiles/pyhanabi.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cawa/Documents/SoSe19/NIP/hanabi/pyhanabi.cc" "/home/cawa/Documents/SoSe19/NIP/hanabi/CMakeFiles/pyhanabi.dir/pyhanabi.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "hanabi_lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cawa/Documents/SoSe19/NIP/hanabi/hanabi_lib/CMakeFiles/hanabi.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
