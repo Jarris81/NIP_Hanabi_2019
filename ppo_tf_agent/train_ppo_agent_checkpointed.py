ad8d58f1369721f5c15c084854ae3a749814b2a5
Alexander Dittmann <alex.dittmann@gmail.com>
2019-06-24 18:24:14 +0200

    checkpointed version can now load saved checkpoints

--- ppo_tf_agent/train_ppo_agent_checkpointed.py
+++ ppo_tf_agent/train_ppo_agent_checkpointed.py
@@ -14,12 +14,15 @@
 # limitations under the License.
 
 r"""Train and Eval PPO.
+
 To run:
+
 ```bash
-tensorboard --logdir $HOME/tmp/ppo/gym/HalfCheetah-v2/ --port 2223 &
-python tf_agents/agents/ppo/examples/v2/train_eval.py \
-  --root_dir=$HOME/tmp/ppo/gym/HalfCheetah-v2/ \
-  --logtostderr
+tensorboard --logdir <SOME_DIR> --port 2223 &
+
+python[3] train_ppo_agent_checkpointed.py  \
+  --root_dir=<SOME_DIR> \
+  --alsologtostderr
 ```
 """
 
@@ -43,10 +46,15 @@ from tf_agents.environments import parallel_py_environment
 from tf_agents.environments import suite_mujoco
 from tf_agents.environments import tf_py_environment
 from tf_agents.eval import metric_utils
+from tf_agents.metrics import batched_py_metric
 from tf_agents.metrics import tf_metrics
+from tf_agents.metrics.py_metrics import AverageEpisodeLengthMetric
+from tf_agents.metrics.py_metrics import AverageReturnMetric
+from tf_agents.networks import actor_distribution_network
 from tf_agents.networks import actor_distribution_rnn_network
 from tf_agents.networks import value_network
 from tf_agents.networks import value_rnn_network
+from tf_agents.policies import py_tf_policy
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.utils import common
 
@@ -57,6 +65,7 @@ import pyhanabi_env_wrapper
 
 flags.DEFINE_string('root_dir', os.getenv('UNDEFINED'),
                     'Root directory for writing logs/summaries/checkpoints.')
+flags.DEFINE_string('master', '', 'master session')
 flags.DEFINE_string('env_name', 'Hanabi-Full', 'Name of an environment')
 flags.DEFINE_integer('replay_buffer_capacity', 1001,
                      'Replay buffer capacity per env.')
@@ -81,6 +90,7 @@ FLAGS = flags.FLAGS
 @gin.configurable
 def train_eval(
     root_dir,
+    tf_master='',
     env_name='Hanabi-Full',
     num_players=4,
     env_load_fn=None,
@@ -103,15 +113,15 @@ def train_eval(
     num_eval_episodes=30,
     eval_interval=500,
     # Params for summaries and logging
-    train_checkpoint_interval=100,
-    policy_checkpoint_interval=50,
-    rb_checkpoint_interval=200,
+    train_checkpoint_interval=2000,
+    policy_checkpoint_interval=1000,
+    rb_checkpoint_interval=4000,
     log_interval=50,
     summary_interval=50,
     summaries_flush_secs=1,
-    use_tf_functions=True,
     debug_summaries=False,
-    summarize_grads_and_vars=False):
+    summarize_grads_and_vars=False,
+    eval_metrics_callback=None):
   """A simple train and eval for PPO."""
   if root_dir is None:
     raise AttributeError('train_eval requires a root_dir.')
@@ -127,15 +137,24 @@ def train_eval(
   eval_summary_writer = tf.compat.v2.summary.create_file_writer(
       eval_dir, flush_millis=summaries_flush_secs * 1000)
   eval_metrics = [
-      tf_metrics.AverageReturnMetric(buffer_size=num_eval_episodes),
-      tf_metrics.AverageEpisodeLengthMetric(buffer_size=num_eval_episodes)
+      batched_py_metric.BatchedPyMetric(
+          AverageReturnMetric,
+          metric_args={'buffer_size': num_eval_episodes},
+          batch_size=num_parallel_environments),
+      batched_py_metric.BatchedPyMetric(
+          AverageEpisodeLengthMetric,
+          metric_args={'buffer_size': num_eval_episodes},
+          batch_size=num_parallel_environments),
   ]
+  eval_summary_writer_flush_op = eval_summary_writer.flush()
 
   global_step = tf.compat.v1.train.get_or_create_global_step()
   with tf.compat.v2.summary.record_if(
       lambda: tf.math.equal(global_step % summary_interval, 0)):
     tf.compat.v1.set_random_seed(random_seed)
-    eval_tf_env = tf_py_environment.TFPyEnvironment(env_load_fn(env_name, num_players))
+    #eval_tf_env = tf_py_environment.TFPyEnvironment(env_load_fn(env_name, num_players))
+    eval_py_env = parallel_py_environment.ParallelPyEnvironment(
+        [lambda: env_load_fn(env_name, num_players)] * num_parallel_environments)
     tf_env = tf_py_environment.TFPyEnvironment(
         parallel_py_environment.ParallelPyEnvironment(
             [lambda: env_load_fn(env_name, num_players)] * num_parallel_environments))
@@ -175,40 +194,47 @@ def train_eval(
         summarize_grads_and_vars=summarize_grads_and_vars,
         train_step_counter=global_step,
         normalize_observations=False) # cause the observations also include the 0-1 mask
-    tf_agent.initialize()
-
-    environment_steps_metric = tf_metrics.EnvironmentSteps()
-    step_metrics = [
-        tf_metrics.NumberOfEpisodes(),
-        environment_steps_metric,
-    ]
-
-    train_metrics = step_metrics + [
-        tf_metrics.AverageReturnMetric(),
-        tf_metrics.AverageEpisodeLengthMetric(),
-    ]
-
-    eval_policy = tf_agent.policy
-    collect_policy = tf_agent.collect_policy
 
     replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
         tf_agent.collect_data_spec,
         batch_size=num_parallel_environments,
         max_length=replay_buffer_capacity)
 
-    collect_driver = dynamic_episode_driver.DynamicEpisodeDriver(
+    #eval_policy = tf_agent.policy
+    eval_py_policy = py_tf_policy.PyTFPolicy(tf_agent.policy)
+
+    environment_steps_metric = tf_metrics.EnvironmentSteps()
+    environment_steps_count = environment_steps_metric.result()
+    step_metrics = [
+        tf_metrics.NumberOfEpisodes(),
+        environment_steps_metric,
+    ]
+    train_metrics = step_metrics + [
+        tf_metrics.AverageReturnMetric(),
+        tf_metrics.AverageEpisodeLengthMetric(),
+    ]
+
+    # Add to replay buffer and other agent specific observers.
+    replay_buffer_observer = [replay_buffer.add_batch]
+
+    collect_policy = tf_agent.collect_policy
+
+    collect_op = dynamic_episode_driver.DynamicEpisodeDriver(
         tf_env,
         collect_policy,
-        observers=[replay_buffer.add_batch] + train_metrics,
-        num_episodes=collect_episodes_per_iteration)
+        observers=replay_buffer_observer + train_metrics,
+        num_episodes=collect_episodes_per_iteration).run()
 
-    if use_tf_functions:
-      # TODO(b/123828980): Enable once the cause for slowdown was identified.
-      collect_driver.run = common.function(collect_driver.run, autograph=False)
-      tf_agent.train = common.function(tf_agent.train, autograph=False)
+    trajectories = replay_buffer.gather_all()
 
+    train_op, _ = tf_agent.train(experience=trajectories)
 
-    # checkpointers
+    with tf.control_dependencies([train_op]):
+      clear_replay_op = replay_buffer.clear()
+
+    with tf.control_dependencies([clear_replay_op]):
+      train_op = tf.identity(train_op)
+
     train_checkpointer = common.Checkpointer(
         ckpt_dir=train_dir,
         agent=tf_agent,
@@ -223,75 +249,95 @@ def train_eval(
         max_to_keep=1,
         replay_buffer=replay_buffer)
 
+    summary_ops = []
+    for train_metric in train_metrics:
+      summary_ops.append(train_metric.tf_summaries(
+          train_step=global_step, step_metrics=step_metrics))
 
-    collect_time = 0
-    train_time = 0
-    timed_at_step = global_step.numpy()
-
-    while environment_steps_metric.result() < num_environment_steps:
-      print("ENVIRONMENT_STEPS: ", environment_steps_metric.result())
-      global_step_val = global_step.numpy()
-      if global_step_val % eval_interval == 0:
-        metric_utils.eager_compute(
-            eval_metrics,
-            eval_tf_env,
-            eval_policy,
-            num_episodes=num_eval_episodes,
-            train_step=global_step,
-            summary_writer=eval_summary_writer,
-            summary_prefix='Metrics',
-        )
-        print('AVG RETURN:', compute_avg_return(eval_tf_env, eval_policy))
-
-      start_time = time.time()
-      collect_driver.run()
-      collect_time += time.time() - start_time
-
-      start_time = time.time()
-      trajectories = replay_buffer.gather_all()
-      total_loss, _ = tf_agent.train(experience=trajectories)
-      replay_buffer.clear()
-      train_time += time.time() - start_time
-      
-      for train_metric in train_metrics:
-        train_metric.tf_summaries(
+    with eval_summary_writer.as_default(), \
+         tf.compat.v2.summary.record_if(True):
+      for eval_metric in eval_metrics:
+        eval_metric.tf_summaries(
             train_step=global_step, step_metrics=step_metrics)
 
-      if global_step_val % log_interval == 0:
-        logging.info('step = %d, loss = %f', global_step_val, total_loss)
-        steps_per_sec = (
-            (global_step_val - timed_at_step) / (collect_time + train_time))
-        logging.info('%.3f steps/sec', steps_per_sec)
-        logging.info('collect_time = {}, train_time = {}'.format(
-            collect_time, train_time))
-        with tf.compat.v2.summary.record_if(True):
-          tf.compat.v2.summary.scalar(
-              name='global_steps_per_sec', data=steps_per_sec, step=global_step)
+    init_agent_op = tf_agent.initialize()
 
-        timed_at_step = global_step_val
-        collect_time = 0
-        train_time = 0
+    with tf.compat.v1.Session(tf_master) as sess:
+      # Initialize graph.
+      train_checkpointer.initialize_or_restore(sess)
+      rb_checkpointer.initialize_or_restore(sess)
+      common.initialize_uninitialized_variables(sess)
 
-      # make checkpoints
-      if global_step_val % train_checkpoint_interval == 0:
-        train_checkpointer.save(global_step=global_step_val)
+      sess.run(init_agent_op)
+      sess.run(train_summary_writer.init())
+      sess.run(eval_summary_writer.init())
 
-      if global_step_val % policy_checkpoint_interval == 0:
-        policy_checkpointer.save(global_step=global_step_val)
+      collect_time = 0
+      train_time = 0
+      timed_at_step = sess.run(global_step)
+      steps_per_second_ph = tf.compat.v1.placeholder(
+          tf.float32, shape=(), name='steps_per_sec_ph')
+      steps_per_second_summary = tf.compat.v2.summary.scalar(
+          name='global_steps_per_sec', data=steps_per_second_ph,
+          step=global_step)
 
-      if global_step_val % rb_checkpoint_interval == 0:
-        rb_checkpointer.save(global_step=global_step_val)
+      while sess.run(environment_steps_count) < num_environment_steps:
+        global_step_val = sess.run(global_step)
+        if global_step_val % eval_interval == 0:
+          metric_utils.compute_summaries(
+              eval_metrics,
+              eval_py_env,
+              eval_py_policy,
+              num_episodes=num_eval_episodes,
+              global_step=global_step_val,
+              callback=eval_metrics_callback,
+              log=True,
+          )
+          sess.run(eval_summary_writer_flush_op)
+          #print('AVG RETURN:', compute_avg_return(eval_tf_env, eval_policy))  ! extremely slow
 
-    # One final eval before exiting.
-    metric_utils.eager_compute(
-        eval_metrics,
-        eval_tf_env,
-        eval_policy,
-        num_episodes=num_eval_episodes,
-        train_step=global_step,
-        summary_writer=eval_summary_writer,
-        summary_prefix='Metrics',
-    )
+        start_time = time.time()
+        sess.run(collect_op)
+        collect_time += time.time() - start_time
+        start_time = time.time()
+        total_loss, _ = sess.run([train_op, summary_ops])
+        train_time += time.time() - start_time
+
+        global_step_val = sess.run(global_step)
+        if global_step_val % log_interval == 0:
+          logging.info('step = %d, loss = %f', global_step_val, total_loss)
+          steps_per_sec = (
+              (global_step_val - timed_at_step) / (collect_time + train_time))
+          logging.info('%.3f steps/sec', steps_per_sec)
+          sess.run(
+              steps_per_second_summary,
+              feed_dict={steps_per_second_ph: steps_per_sec})
+          logging.info('%s', 'collect_time = {}, train_time = {}'.format(
+              collect_time, train_time))
+          timed_at_step = global_step_val
+          collect_time = 0
+          train_time = 0
+
+        if global_step_val % train_checkpoint_interval == 0:
+          train_checkpointer.save(global_step=global_step_val)
+
+        if global_step_val % policy_checkpoint_interval == 0:
+          policy_checkpointer.save(global_step=global_step_val)
+
+        if global_step_val % rb_checkpoint_interval == 0:
+          rb_checkpointer.save(global_step=global_step_val)
+
+      # One final eval before exiting.
+      metric_utils.compute_summaries(
+          eval_metrics,
+          eval_py_env,
+          eval_py_policy,
+          num_episodes=num_eval_episodes,
+          global_step=global_step_val,
+          callback=eval_metrics_callback,
+          log=True,
+      )
+      sess.run(eval_summary_writer_flush_op)
 
 
 def load_hanabi_env(env_name="Hanabi-Full", num_players=4):
@@ -307,12 +353,16 @@ def compute_avg_return(environment, policy, num_episodes=30):
         episode_return = 0.0
         policy_state = policy.get_initial_state(1)
 
-        while not time_step.is_last():
+        print('TIME STEP', time_step)
+        print('TIME STEP', time_step.is_last().eval()[0])
+        print('POLICY STATE', policy_state)
+
+        while not time_step.is_last().eval()[0]:
             policy_step = policy.action(time_step, policy_state)
             policy_state = policy_step.state
             time_step = environment.step(policy_step.action)
 
-            if not time_step.is_last():
+            if not time_step.is_last().eval()[0]:
                 episode_return += time_step.reward
 
         total_return += episode_return
@@ -322,21 +372,26 @@ def compute_avg_return(environment, policy, num_episodes=30):
     return avg_return.numpy()[0]
 
 def main(_):
+  tf.compat.v1.enable_resource_variables()
+  if tf.executing_eagerly():
+    # self.skipTest('b/123777119')  # Secondary bug: ('b/123775375')
+    return
+
   logging.set_verbosity(logging.INFO)
-  tf.compat.v1.enable_v2_behavior()
   train_eval(
       FLAGS.root_dir,
+      tf_master=FLAGS.master,
       env_name=FLAGS.env_name,
-      use_rnns=FLAGS.use_rnns,
+      replay_buffer_capacity=FLAGS.replay_buffer_capacity,
       env_load_fn=load_hanabi_env,
       num_environment_steps=FLAGS.num_environment_steps,
-      collect_episodes_per_iteration=FLAGS.collect_episodes_per_iteration,
       num_parallel_environments=FLAGS.num_parallel_environments,
-      replay_buffer_capacity=FLAGS.replay_buffer_capacity,
       num_epochs=FLAGS.num_epochs,
-      num_eval_episodes=FLAGS.num_eval_episodes)
+      collect_episodes_per_iteration=FLAGS.collect_episodes_per_iteration,
+      num_eval_episodes=FLAGS.num_eval_episodes,
+      use_rnns=FLAGS.use_rnns)
 
 
 if __name__ == '__main__':
   flags.mark_flag_as_required('root_dir')
-app.run(main)
+  app.run(main)
