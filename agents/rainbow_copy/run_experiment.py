94e519fdf8b094149787f6dd97c539a2271b0c96
dg <dgrinwald93@gmail.com>
2019-07-27 17:58:33 +0200

    refactor

--- agents/rainbow_copy/run_experiment.py
+++ agents/rainbow_copy/run_experiment.py
@@ -29,16 +29,13 @@ from __future__ import print_function
 
 import time
 
-fp = str(__file__)[0:-len('agents/simple_agent.py')]
-sys.path.insert(1, fp+'hanabi_learning_environment/')
-
-from third_party.dopamine import checkpointer
-from third_party.dopamine import iteration_statistics
-import dqn_agent
+from hanabi_learning_environment.agents.rainbow.third_party.dopamine import checkpointer
+from hanabi_learning_environment.agents.rainbow.third_party.dopamine import iteration_statistics
+import hanabi_learning_environment.rl_env as rl_env
+import agents.rainbow.dqn_agent as dqn_agent
 import gin.tf
-import rl_env
 import numpy as np
-import rainbow_agent
+import agents.rainbow.rainbow_agent as rainbow_agent
 import tensorflow as tf
 
 LENIENT_SCORE = False
